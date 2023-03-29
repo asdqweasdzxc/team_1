13dfc800c3db031279dbe47f6abe452ebd086179
jd0601 <syd02110@naver.com>
2023-03-29 10:58:12 +0900

    'shop_maincontroller내용추가'

--- src/main/java/team1/project/bookshop/shop/controller/MainController.java
+++ src/main/java/team1/project/bookshop/shop/controller/MainController.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import team1.project.bookshop.model.book.BookService;
 import team1.project.bookshop.model.inquiry_category.Inquiry_categoryService;
+import team1.project.bookshop.util.PageManager2;
 
 //쇼핑몰의 메인을 처리하는 하위 컨트롤러
 
@@ -18,6 +20,9 @@ public class MainController {
 	
 	@Autowired
 	private Inquiry_categoryService inquiry_categoryService;
+	
+	@Autowired
+	private BookService bookService;
 	
 	@GetMapping("/")
 	public ModelAndView getMain(HttpServletRequest request) {
@@ -28,6 +33,21 @@ public class MainController {
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("inquiry_categoryList", inquiry_categoryList);
 		mav.setViewName("shop/index");
+		
+		return mav;
+	}
+	
+	@GetMapping("/book/search")
+	public ModelAndView search() {
+		
+		List bookList = bookService.selectAll();
+		PageManager2 pageManager = new PageManager2();
+		pageManager.init(bookList);
+		
+		
+		ModelAndView mav = new ModelAndView("client/booksearch2");
+		mav.addObject("bookList", bookList);
+		mav.addObject("pageManager", pageManager);
 		
 		return mav;
 	}
